bc7bea117049457b00a993e910330e3e60afe172
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-07-08 20:36:14 +0530

    Add files via upload

---
+++ operators_in_python.py
@@ -0,0 +1,62 @@
+''' operators in python'''
+
+''' Arthemetic operators'''
+
+# a,b= (int(w) for w in input ("Enter the value of a and b :").split())
+# print(f"The value of {a} + {b} = {a+b}")                                    # The value of 12 +  25 = 37
+# print(f"The value of {a} * {b} = {a*b}")                                    # The value of 10 * 23 = 230
+# print(f"The value of {a} - {b} = {a-b}")                                    # The value of 10 - 23 = -13
+# print(f"The value of {a} / {b} = {a/b}")                                    # The value of 10 / 23 = 0.43478260869565216
+# print(f"The value of {a} ** {b} = {a**b}")                                  # The value of 10**23 = 100000000000000000000000
+# print(f"The value of {a} % {b} = {a%b}")                                      # The value of 10 % 45 = 10
+
+''' floor division 
+In this type if the two values are int then we will get the o/p as int only
+      if we are taking the values as one int and one float the o/p will be float'''
+
+# c =4// 2
+# print(c)                                                                        # 2 
+# e = 8//3
+# print(e)                                                                          # 2
+# d = 10/5
+# print(d)                                                                          # 2.0
+# w = 6.0//3
+# print(w)                                                                            # 2.0
+# p = 6.8//2.3
+# print(p)                                                                              # 2.0
+# q = 5.0/2.0                                                                        
+# print(q)                                                                                # 2.5                                                                          # 2.5
+
+
+
+'''assignment operators'''
+# a = 5 ; b = 14
+# a= a+b
+# print(a)                                                                            # 19
+
+
+
+
+# a = 23
+# a+= 20
+# print(a)                                                                                # 43
+
+
+
+# a+= -2
+# print(a)                                                                                    # 21
+
+# a-= 10                                                     #  ( a+ = a + number given)
+# print(a)                                                        # 13
+
+# a*= -5
+# print(a)                                                                    # 23*-5 = -115
+
+
+# v=int(input("Enter the value of v :"))
+# print(f"The value of v is {v}")                                                   # 10
+# v+= 2
+# print("The value of v after q+2 is {} :".format(v))                             # The value of q after q+2 is 10 :
+                                                                        
+
+print("The value of p after p-4 is {p} : ".format(p=20))
